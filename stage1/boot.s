d1b32a390da75b931eec160a49a322aba73afc8b
Bodanor <papadopoulos.chris@icloud.com>
2023-01-31 14:36:24 +0100

    Added function enable A20 Gate with the keyboard and jumping to the next sector

--- stage1/boot.s
+++ stage1/boot.s
@@ -51,18 +51,62 @@ _init:
 	mov bp, 0x7c00
 	mov sp, bp
 	
-
-.SetVideoMode:
-	mov ah, 0x0
-	mov al, 0x3
-	int 0x10
-	
-	mov bx, offset flat:VideoModeMsg
-	call print_string
 	
 	mov bx, offset flat:FirstStageMsg
 	call print_string
 
+
+	call .checkA20_Line
+	cmp al, 0
+	jne .AfterA20Enabled
+	
+	mov bx, offset flat:A20DisabledMsg
+	call print_string
+	
+	/* Enable A20 with BIOS Interrupt */
+
+	call .enableA20Gate_BIOS
+	call .checkA20_Line
+	cmp al, 0
+	jne .AfterA20Enabled
+	/* Enable A20 with Keyboard Method */
+	
+	call .enableA20Gate_Keyboard
+	call .checkA20_Line
+	cmp al, 0
+	jne .AfterA20Enabled
+	
+	mov bx, offset flat:A20FatalErrorMsg
+	call print_string
+
+BootError:
+	hlt
+	jmp .
+
+.AfterA20Enabled: /* One of the above function to enable A20 worked */
+	mov bx, offset flat:A20EnabledMsg
+	call print_string
+	jmp .
+
+.LoadNextSector:
+	xor ax, ax
+	mov es, ax
+	mov bx, 0x7e00
+	mov dl, 1
+	push dx
+	mov ah, 0x2
+	mov al, dl
+	mov cl, 0x2
+	mov ch, 0x00
+	mov dh, 0x00
+
+	int 0x13
+	jc BootError
+	pop dx
+	cmp al, dh
+	jne BootError
+
+	jmp 0x7e00
 
 
 /* 	This routines check wether the A20 line is enabled or not
@@ -85,8 +129,10 @@ _init:
 	cmp al, byte ptr es:[si] # Did memory wrapped around ? If so, A20line is not enabled, we should do so !!!
 	
 	mov bx, 1
-	jne .skip_a20
-	
+	jne .potentielly_not_enabled
+	mov bx, 0
+
+.potentielly_not_enabled:
 	/* Here we check if we didn't compare with the same value by mere chance. So we actually change the value and cmp again */
 	mov byte ptr ds:[di], 0xff
 	mov al, byte ptr ds:[di]
@@ -95,26 +141,93 @@ _init:
 	pop ax
 	mov byte ptr ds:[di], al
 
-	jne .skip_a20
-	mov bx, 0
+	je .skip_a20
+	mov bx, 1
 
 .skip_a20:
-
-		ret
+	mov ax, bx
+	ret
 
 /* This is a routine to enable the a20 line if not already enabled ! */
 
-.enableA20Gate:
+.enableA20Gate_BIOS:
+	mov ax, 0x2403
+	int 0x15
+	jb .a20_bios_not_supported
+	cmp ah, 0
+	jnz .a20_bios_not_supported
 	
+	mov ax, 0x2402
+	int 0x15
+	jb .a20_bios_not_supported
+	cmp ah, 0
+	jnz .a20_bios_not_supported
+
+	cmp al, 1
+	jz .a20Gate_BIOS_End
+
+	mov ax, 0x2401
+	int 0x15
+	jb .a20_bios_not_supported
+	cmp ah, 0
+	jnz .a20_bios_not_supported
+	jmp .a20Gate_BIOS_End
+
+.a20_bios_not_supported:
+
+.a20Gate_BIOS_End:
+	ret
+
+.enableA20Gate_Keyboard:
+	cli
+	call .a20wait
+	mov al, 0xAD
+	out 0x64, al
+
+	call .a20wait
+	mov al, 0xD0
+	out 0x64, al
+	
+	call .a20wait2
+	in al, 0x60
+	push eax
+	
+	call .a20wait
+	mov al, 0xD1
+	out 0x64, al
+
+	call .a20wait
+	pop eax
+	or al, 2
+	out 0x60, al
+
+	call .a20wait
+	mov al, 0xAE
+	out 0x64, al
+
+	call .a20wait
+	sti
+	ret
+.a20wait:
+	in al, 0x64
+	test al, 2
+	jnz .a20wait
+	ret
+
+.a20wait2:
+	in al, 0x64
+	test al, 1
+	jnz .a20wait2
+	ret
 
 FirstStageMsg:
 	.asciz "First Stage loaded !\n"
-VideoModeMsg:
-	.asciz "Changing Video mode to 80x25 16bit color\n"
 A20EnabledMsg:
 	.asciz "A20 Gate enabled !\n"
 A20DisabledMsg:
-	.asciz "A20 Gate disabled, trying to enable it\n"
+	.asciz "A20 Gate disabled\n"
+A20FatalErrorMsg:
+	.asciz "Could not enable A20 Gate\nAbording BOOT !"
 bootDrive:
 	.byte 0
 
