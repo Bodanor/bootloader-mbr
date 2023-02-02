0ff73edd47128468c0760651e55b4b2e7a89131d
Bodanor <papadopoulos.chris@icloud.com>
2023-02-02 23:07:53 +0100

    IDK how to include smbols from one sections to another without declaring every single label global..

--- stage1/boot-next.s
+++ stage1/boot-next.s
@@ -1,6 +1,6 @@
 	.intel_syntax noprefix
 	.code16
-	.section .stage1_nxt, "ax"
+	.section ".stage1_nxt", "ax"
 
 .stage1_next:
 	mov bx, offset flat:NextSectorMsg
@@ -9,9 +9,18 @@
 
 
 
+.compute_root_sectors:
+	mov ax, 32
+	xor dx, dx
+	mul word ptr iRootSize
 
 NextSectorMsg:
 	.asciz "Next sector !\n"
-	
+
+root_sectors:
+	.word 0
+root_start_pos:
+	.int 0
+
 
 
